50a77eb82906a9e02fde57653fd18ae45150d19b
Kosmonautt <b.contreras.2@ug.uchile.cl>
2023-09-01 21:07:42 -0400

    Both players get created succesfully but both of them get controlled by the same player.

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,17 +2,20 @@ extends Node3D
 
 @export var player_scene: PackedScene
 @onready var players: Node3D = $Players
-
+@onready var spawn: Node3D = $Spawn
 
 func _ready() -> void:
 	# a playable character is created for each player
-	for player_data in Game.players:
-		# we instantiate a character
+	for i in Game.players.size():
+		# we get the palyer data
+		var player_data = Game.players[i]
+		# we instantiate a player
 		var player = player_scene.instantiate()
 		# it gets added as a child of "players" node
 		players.add_child(player)
 		# setup for authority and other settings
-		player.setup(player_data)
+		#player.setup(player_data)
+		player.global_position = spawn.get_child(i).global_position
 	
 
 
