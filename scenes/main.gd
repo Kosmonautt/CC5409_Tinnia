c279d8890a6e4bdcacf1365296cf25dc8cbabb39
Kosmonautt <b.contreras.2@ug.uchile.cl>
2023-09-01 20:42:32 -0400

    Changed main and main.gd from 2D to 3D and deleted character instance in stage.

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,13 +1,17 @@
-extends Node2D
+extends Node3D
 
 @export var player_scene: PackedScene
-@onready var players: Node2D = $Players
+@onready var players: Node3D = $Players
 
 
 func _ready() -> void:
+	# a playable character is created for each player
 	for player_data in Game.players:
+		# we instantiate a character
 		var player = player_scene.instantiate()
+		# it gets added as a child of "players" node
 		players.add_child(player)
+		# setup for authority and other settings
 		player.setup(player_data)
 	
 
