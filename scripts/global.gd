593eb0a1f8c87d9bbcb64b94097f314f4a92be0d
Reik0z <demian.hott@ug.uchile.cl>
2023-10-14 10:56:11 -0300

    order folders

--- scripts/global.gd
+++ scripts/global.gd
@@ -18,7 +18,7 @@ func _process(_delta):
 func game_ready() -> void:
 	players_alive = Game.players.map(func(value): return value.id)
 	add_child(global_timer)
-	global_timer.start(30)
+	global_timer.start(10)
 	# manage the timer on the multiplayer authority
 	if is_multiplayer_authority():
 		global_timer.timeout.connect(_on_global_timer_timeout)
@@ -38,15 +38,19 @@ func emit_die() -> void:
 func _on_global_timer_timeout():
 	emit_die.rpc_id(bomb_carrier)
 	players_alive.erase(bomb_carrier)
-#	if players_alive.size() == 1:
-#		Debug.dprint("GANASTE")
-#	else:
-	# nuevo jugador que tiene la bomba
-	var i : int = players_alive[randi() % players_alive.size()]
-	update_the_bomb.rpc(i)
+	if players_alive.size() == 1:
+		game_end.rpc()
+	else:
+		var i : int = players_alive[randi() % players_alive.size()]
+		update_the_bomb.rpc(i)
 
 func is_player_alive(player_id : int) -> bool:
 	for i in players_alive:
 		if i == player_id:
 			return true
 	return false
+
+@rpc("any_peer", "call_local", "reliable")
+func game_end():
+	get_tree().change_scene_to_file("res://scenes/ui/endscreen.tscn")
+	global_timer.stop()
