1d144bb139e536a19155203cdeb77c36eff5fc9e
Reik0z <demian.hott@ug.uchile.cl>
2023-10-16 00:44:37 -0300

    added barbarian and fixed showing mouse in endscreen

--- scripts/endscreen.gd
+++ scripts/endscreen.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var winner_label : Label = $ControlScreen/MarginContainer/VBoxContainer2/VBoxContainer/WinnerLabel
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var mainmenu_button : Button = $ControlScreen/MarginContainer/VBoxContainer2/VBoxContainer2/mainmenu_bottom
+var showing_mouse : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +13,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_cancel"):
+		if not showing_mouse:
+			# the mouse becomes visible so we can click the X
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			showing_mouse = true
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			showing_mouse = false
 
 
 func _on_mainmenu_bottom_pressed():
