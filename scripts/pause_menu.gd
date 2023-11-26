157afdd85242ac093582d9f6e86688d24381d50c
Reik0z <demian.hott@ug.uchile.cl>
2023-11-25 21:16:10 -0300

    added knight power

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -11,7 +11,7 @@ func _ready():
 	Game.paused.connect(_on_game_paused) # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _input(event):
